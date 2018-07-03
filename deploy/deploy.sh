78fbff70661e9d843c89d334e74b8bb641fd3fe1
nestor <nestor.wbpark@gmail.com>
2018-07-03 17:02:03 +0900

    zcp-demo 배포용으로 namespace및 domain 수정

--- deploy/deploy.sh
+++ deploy/deploy.sh
@@ -3,22 +3,22 @@ echo ""
 echo "=============================="
 echo "docker:build"
 echo "=============================="
-mvn clean install -Dmaven.test.skip=true docker:build
+mvn clean install -P zcp-demo -Dmaven.test.skip=true docker:build
 
 #bx cr login
 echo ""
 echo "=============================="
 echo "        tagging, push"
 echo "=============================="
-docker image tag zdb-portal:latest registry.au-syd.bluemix.net/zdb-dev/zdb-portal:latest
-docker image push registry.au-syd.bluemix.net/zdb-dev/zdb-portal:latest
+docker image tag zdb-portal-ui:latest registry.au-syd.bluemix.net/zdb-dev/zdb-portal-ui:latest
+docker image push registry.au-syd.bluemix.net/zdb-dev/zdb-portal-ui:latest
 
 bx cr image-list
 echo ""
 echo "=============================="
 echo "del old version service [zdb-portal-deployment]"
 echo "=============================="
-kubectl delete deployment zdb-portal-deployment -n zdb
+kubectl delete deployment zdb-portal-deployment -n zdb-system
 echo ""
 echo "=============================="
 echo "sleep 5s"
@@ -28,4 +28,4 @@ echo ""
 echo "=============================="
 echo "create new version service [zdb-portal-deployment]"
 echo "=============================="
-kubectl apply -f deployment.yml
+kubectl apply -f zdb-system-deployment.yml
