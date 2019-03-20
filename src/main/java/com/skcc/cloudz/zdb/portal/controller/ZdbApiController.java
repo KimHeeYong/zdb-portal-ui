48d21ebcb0d761655300a778158c57df15e2d96f
dwdwppp <dwdwppp@nate.com>
2019-03-20 15:41:42 +0900

    서비스 상세 그리드crud 적용

--- src/main/java/com/skcc/cloudz/zdb/portal/controller/ZdbApiController.java
+++ src/main/java/com/skcc/cloudz/zdb/portal/controller/ZdbApiController.java
@@ -1,7 +1,5 @@
 package com.skcc.cloudz.zdb.portal.controller;
 
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.forwardedUrl;
-
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
