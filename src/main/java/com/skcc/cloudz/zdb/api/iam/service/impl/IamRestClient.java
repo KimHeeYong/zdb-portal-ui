7f5e32dc79024d4b673cd255f69b61b2788d65ab
박남수(Park Nam-Soo)/플랫폼Operation그룹/SK <pns@sk.com>
2019-08-14 17:36:38 +0900

    사용자 인증 체크 로직 추가

--- src/main/java/com/skcc/cloudz/zdb/api/iam/service/impl/IamRestClient.java
+++ src/main/java/com/skcc/cloudz/zdb/api/iam/service/impl/IamRestClient.java
@@ -1,6 +1,9 @@
 package com.skcc.cloudz.zdb.api.iam.service.impl;
 
 import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
 
 import org.codehaus.jackson.map.ObjectMapper;
 import org.slf4j.Logger;
@@ -13,20 +16,30 @@ import org.springframework.http.HttpMethod;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
 import org.springframework.stereotype.Service;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.client.HttpStatusCodeException;
 import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.util.UriComponentsBuilder;
 
 import com.skcc.cloudz.zdb.api.iam.domain.vo.ApiResponseVo;
+import com.skcc.cloudz.zdb.common.exception.KeyCloakException;
 
 @Service
 public class IamRestClient {
     
     private static final Logger log = LoggerFactory.getLogger(IamRestClient.class);
     
+    private static final String realm = "zcp";
+    
     @Value("${props.iam.baseUrl}")
     private String iamBaseUrl;
+    
+    @Value("${props.keycloak.zcp.accessTokenUri}")
+    private String accessTokenUri;
     
     @Autowired
     private RestTemplate restTemplate;
@@ -72,6 +85,36 @@ public class IamRestClient {
         return apiResponseVo;
     }
 
-    
+    public String getAccessToken(String userId, String password) throws KeyCloakException {
+		try {
+			HttpComponentsClientHttpRequestFactory httpRequestFactory = new HttpComponentsClientHttpRequestFactory();
+	        httpRequestFactory.setConnectionRequestTimeout(1000);
+	        httpRequestFactory.setConnectTimeout(1000);
+	        httpRequestFactory.setReadTimeout(1000);
+			
+			RestTemplate restTemplate = new RestTemplate(httpRequestFactory);
+			
+			MultiValueMap<String, String> body = new LinkedMultiValueMap<String, String>();
+			body.add("username", userId);
+			body.add("password", password);
+			body.add("grant_type", "password");
+			body.add("client_id", "admin-cli");
+
+			HttpHeaders headers = new HttpHeaders();
+			headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
+
+			HttpEntity<Map<?,?>> entity = new HttpEntity<Map<?,?>>(body, headers);
+
+			// For print IO, change log-level(DEBUG) of RestTemplate or org.spring package.  
+			ResponseEntity<HashMap> res = restTemplate.exchange(accessTokenUri, HttpMethod.POST, entity, HashMap.class, realm);
+			//logger.debug("Success to get access_token.", res);
+			return Objects.toString(res.getBody().get("access_token"));
+		} catch (HttpStatusCodeException e) {
+			if(HttpStatus.valueOf(e.getRawStatusCode()) == HttpStatus.UNAUTHORIZED) {
+				throw new KeyCloakException("KK-002", "The password is incorrect.");
+			}
+			throw e;
+		}
+	}
 
 }
