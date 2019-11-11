a4b9b2038821f6237acea0cfb5b62b85d9c367ca
kumocomms <kumocomms@naver.com>
2019-11-11 17:18:27 +0900

    다국어 처리 -> header 에 locale 값 셋팅 로직 수정 적용

--- src/main/java/com/skcc/cloudz/zdb/common/component/ZdbRestConnector.java
+++ src/main/java/com/skcc/cloudz/zdb/common/component/ZdbRestConnector.java
@@ -142,12 +142,6 @@ public class ZdbRestConnector extends RestTemplate{
 			headers.set("enabled", userInfo.getEnabled() == null ? "false" : userInfo.getEnabled()+"");
 			headers.set("zdbAdmin", userInfo.getZdbAdmin() == null ? "false" : userInfo.getZdbAdmin()+"");
 			// Session Locale 값 적용 (2019-10-31)
-			// server쪽 한국어 코드는 kr을 사용해서 header로 보낼때는 kr로 보내야 함
-			//String locale = (String) session.getAttribute("acceptLanguage") ;
-			if("ko".equals(locale)) locale = "kr"  ;
-			//if("ko".equals(locale)) locale = "ko_KR"  ;
-			//else if("en".equals(locale)) locale = "en_US"  ; 
-
 			headers.set("Accept-Language",locale);
 		}
 	}
